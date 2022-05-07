04418a17b28e19a5f37cbeb08501eae2f56af304
zkx <472434702@qq.com>
2022-05-07 20:37:54 +0800

    second commit

--- src/main/java/com/zkx/git/GitTest.java
+++ src/main/java/com/zkx/git/GitTest.java
@@ -10,5 +10,6 @@ package com.zkx.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("helloGit");
+        System.out.println("helloGit2");
     }
 }
