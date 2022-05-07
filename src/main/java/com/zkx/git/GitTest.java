8833d0fd1c8310c96a340a315991ab96af6bed17
zkx <472434702@qq.com>
2022-05-07 20:36:02 +0800

    first commit

---
+++ src/main/java/com/zkx/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.zkx.git;
+
+/**
+ * @title: GitTest
+ * @Author Zkx
+ * @Date: 2022/5/7 20:32
+ * @Version 1.0
+ */
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("helloGit");
+    }
+}
